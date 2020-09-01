598eead5fd3e069469cddfcb5b6bb1ee1be51df9
Denzel Code <denzelcodedev@gmail.com>
2020-09-01 12:40:57 -0400

    default boolean value

--- src/main/java/com/code/advancedsql/query/column/Boolean.java
+++ src/main/java/com/code/advancedsql/query/column/Boolean.java
@@ -4,7 +4,7 @@ import com.code.advancedsql.query.action.IAction;
 
 public class Boolean extends Column {
 
-    protected java.lang.String defaultValue;
+    protected java.lang.Boolean defaultValue;
 
     public Boolean(IAction<IColumn> action, java.lang.String name) {
         super(action, name, false);
@@ -15,7 +15,7 @@ public class Boolean extends Column {
      * @param value Default value.
      * @return Column object.
      */
-    public Boolean defaultValue(java.lang.String value) {
+    public Boolean defaultValue(java.lang.Boolean value) {
         this.defaultValue = value;
 
         return this;
